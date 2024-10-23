8de358392a8b8beacba56153a740888d39299bc2
Andrii <88sushilnikov@gmail.com>
2024-10-23 03:45:07 +0300

    task-2 v1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -30,12 +30,17 @@ const options = {
     }
   },
 };
+
 const fp = flatpickr(selector, options);
 
 button.addEventListener('click', startTimer);
 
 function startTimer(evt) {
   evt.preventDefault();
+
+  // Вимкнення інпуту та кнопки після старту
+  button.disabled = true;
+  selector.disabled = true;
 
   let interval = setInterval(function () {
     let differenceMs = userSelectedDate - Date.now();
@@ -44,6 +49,10 @@ function startTimer(evt) {
     } else {
       clearInterval(interval);
       updateTimer({ days: '00', hours: '00', minutes: '00', seconds: '00' });
+
+      // Відновлення інпуту після завершення таймера
+      selector.disabled = false;
+
       iziToast.show({
         title: 'Done',
         message: 'Countdown finished!',
@@ -67,24 +76,15 @@ function updateTimer(convertedTime) {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
